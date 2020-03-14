661dfa01a6d8930a1d07b05c37caaf5bf42ac586
wanglifa <wanglifa1995@gmail.com>
2020-03-14 17:32:30 +0800

    useReducer搭建完成

--- src/index/App.tsx
+++ src/index/App.tsx
@@ -1,4 +1,3 @@
-import { connect } from "react-redux";
 import './App.css'
 import * as React from "react";
 import Header from '../common/Header'
@@ -7,8 +6,16 @@ import HighSpeed from "./HighSpeed";
 import Journey from "./Journey";
 import Submit from "./Submit";
 import {createContext, Dispatch, useReducer} from "react";
+import * as actionCreator from './action'
 interface State {
-
+  from: string;
+  to: string;
+  isCitySelectorVisible: boolean;
+  currentSelectingLeftCity: boolean;
+  cityData: any;
+  isLoadingCityData: boolean;
+  isDateSelectorVisible: boolean;
+  highSpeed: boolean;
 }
 interface Action extends State {
   type: string
@@ -18,9 +25,34 @@ interface Context {
   state: State;
   dispatch: Dispatch<Action>
 }
-const store = {}
+const store: State = {
+  from: '北京',
+  to: '上海',
+  isCitySelectorVisible: false,
+  currentSelectingLeftCity: false,
+  cityData: null,
+  isLoadingCityData: false,
+  isDateSelectorVisible: false,
+  highSpeed: false
+}
 const reducer: Reducer = (state, action) => {
   switch (action.type) {
+    case actionCreator.ACTION_SET_CITY_DATA:
+      return { ...state, cityData: action.cityData }
+    case actionCreator.ACTION_SET_CURRENT_SELECTING_LEFT_CITY:
+      return { ...state, currentSelectingLeftCity: action.currentSelectingLeftCity }
+    case actionCreator.ACTION_SET_FROM:
+      return { ...state, from: action.from }
+    case actionCreator.ACTION_SET_HIGH_SPEED:
+      return { ...state, highSpeed: action.highSpeed }
+    case actionCreator.ACTION_SET_IS_CITY_SELECTOR_VISIBLE:
+      return { ...state, isCitySelectorVisible: action.isCitySelectorVisible }
+    case actionCreator.ACTION_SET_IS_DATE_SELECTOR_VISIBLE:
+      return { ...state, isDateSelectorVisible: action.isDateSelectorVisible }
+    case actionCreator.ACTION_SET_IS_LOADING_CITY_DATA:
+      return { ...state, isLoadingCityData: action.isLoadingCityData }
+    case actionCreator.ACTION_SET_TO:
+      return { ...state, to: action.to }
     default:
       throw new Error()
   }
